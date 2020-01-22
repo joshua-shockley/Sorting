2f328a2d6b4f053b99026b4741bbec655bbcb5c9
Joshua-Shockley <thelumbashockley@gmail.com>
2020-01-21 21:52:56 -0600

    almost done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,58 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+import random
+
+# TO-DO: Complete the selection_sort() function below
+
+
+def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+    for i in range(0, len(arr)-1):
+        smallest = min(arr[i:])
+        #         # TO-DO: find next smallest element
+        #         # (hint, can do in 3 loc)
+        sml_index = arr.index(smallest)
+        # loca1, loca2 = value2, value1 to swap values witout temp storage
+        arr[sml_index], arr[i] = arr[i], arr[sml_index]
     return arr
 
+
+def selection_sort2(arr):
+    for i in range(0, len(arr)-1):
+        current_i = i
+        smlest = current_i
+        for nA in range(i+1, len(arr)):
+            if arr[nA] < arr[smlest]:  # as passes comparing the new loop to the set previously smallest
+                smlest = nA
+        # the swap below needs to be assigned under the inner loop so it actually does shit
+                arr[current_i], arr[smlest] = arr[smlest], arr[current_i]
+                print(arr)
+    return arr
+
+
+the_array = [3, 2, 4, 5, 1, 6, 8, 7, 9, 0]
+# selection_sort3(the_array)
+# or
+# l = list(range(10))
+# random.shuffle(l)
+# the above creates a random unordered list of numbers 0-9
+print("selection", selection_sort2(the_array))
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+
+
+def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        for b in range(i + 1, len(arr)):
+            if arr[i] > arr[b]:
+                arr[i], arr[b] = arr[b], arr[i]
 
     return arr
 
 
+print("bubble", bubble_sort(the_array))
+
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+
+
+def count_sort(arr, maximum=-1):
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,53 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
 
-   return -1   # not found
+    # TO-DO: add missing code
+    for item in range(len(arr)):
+        if arr[item] == target:
+            return item
+
+    return -1   # not found
 
 
-# STRETCH: write an iterative implementation of Binary Search 
+# arr = [44, 3, 2, 5, 66, -1, 55, 43, 21, 53, 1, 5, 6]
+# print("linear", linear_search(arr, 33))
+# print("linear", linear_search(arr, -1))
+
+# STRETCH: write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1  # array empty
 
-  # TO-DO: add missing code
+    low = 0
+    high = len(arr)-1
 
-  return -1 # not found
+    # TO-DO: add missing code
+    for low in range(low, high):
+        mid = (low + high) // 2
+        if target == arr[mid]:
+            # print(arr[target], target)
+            return int(arr[target])
+        elif target < arr[mid]:
+            # print('in the first half of range')
+            high = mid - 1
+        else:
+            # print('in the second half of range')
+            low = mid + 1
+        return -1  # notfound
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# print("binary", binary_search(arr, 33))
+# print("binary", binary_search(arr, -1))
+# STRETCH: write a recursive implementation of Binary Search
+
+
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    middle = (low+high)//2
+
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
